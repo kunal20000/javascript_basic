1816455c5d2c24c1498f200e3605b955f0acada8
kunal20000 <kunalsapate2000@gmail.com>
2023-04-28 12:36:16 +0530

    commit

---
+++ src/Assignment/Calculate.js
@@ -0,0 +1,20 @@
+function calculate(num1,num2,operator)
+{
+    if(operator=="+")
+    {
+        console.log(num1+num2);
+    }
+    else if(operator=="-")
+    {
+        console.log(num1-num2);
+    }
+    else if(operator=="*")
+    {
+        console.log(num1*num2);
+    }else if(operator=="/")
+    {
+        console.log(num1/num2);
+    }
+
+}
+calculate();
