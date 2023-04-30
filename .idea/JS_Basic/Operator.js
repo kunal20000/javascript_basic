e831646fcef8549339880b1c180c920939fc4ba8
kunal20000 <kunalsapate2000@gmail.com>
2023-04-30 17:31:49 +0530

    commit

--- .idea/JS_Basic/Operator.js
+++ .idea/JS_Basic/Operator.js
@@ -24,3 +24,144 @@ console.log(a--); //22
 // console.log(++a);
 // console.log(a);
 //
+
+//assignment operator
+
+var z =116;
+console.log(z);
+z=z+10;
+console.log(z);
+z=z*2;
+console.log(z);
+z=z/2;
+console.log(z);
+z=z%2;
+console.log(z);
+
+// comparison operators --> boolean values
+ // for basic comparison '==' or '==='
+
+ console.log(1==1);
+ console.log(1=="1");//in == equal to he check only value
+ console.log(1==="1"); // in === he check vale with type
+
+console.log('kunal'=='kunal');
+ console.log('kunal'==='kunal');
+
+ // --> if value is equal to 1 and add 10 to it --> 11
+
+ console.log('1' == 1); //true --> compares only the values by doing type conversion automatically
+ console.log('1' === 1); //false --> compare the value and type
+
+ console.log(typeof null);
+ console.log(null==null);
+ console.log(null===null);
+ console.log(undefined == 'undefined'); // he checks type
+ console.log(undefined === 'undefined');
+
+ console.log(true == "1");
+ console.log(true === "1");
+
+ console.log(1!= "2"); //check only for value
+ console.log(1!== 2); // check type and value
+
+
+// greater than - >
+// less than - <
+// greater than and equal - >=
+// less athn and equal - <=
+
+console.log(4>7);//false
+console.log(4<7); //t
+console.log(4<=7); //true
+console.log(7>=7); // true
+
+// logical operators
+// AND --> &&
+// OR --> ||
+// NOT --> !
+
+const val1 = 7>9 && 5>6 // false && true --> false
+console.log(val1);
+const val2 = 7>6 || 15<6
+console.log(val2); //true
+
+const val3 = 'abhi' === 'abhi' && 13 >=2 && "ab" == "ab"
+console.log(val3);
+
+const val4 = '1' == 5 || 13 >=20 || true == '1'
+console.log(val4);
+
+// exam -1
+// pass student only if their marks is more than or equals 50
+// and time taken should be less than 25
+
+
+//Ternary Operator
+var studentMarks = 60;
+var timeTaken = 20;
+// before ? returns true or false value
+
+const result1 = studentMarks>=50 && timeTaken<25? 'pass':'fail';
+console.log(result1);
+
+// exam-2
+// pass student only if their marks is more than 60
+// or time taken should be less than 25
+
+var passingMarks = 100;
+var timeTakenInSec = 30;
+
+const result2 = passingMarks>=60 || timeTakenInSec<30 ? 'pass':'fail';
+console.log(result2);
+
+// bitwise operator
+// AND --> & -->
+// OR --> |
+// XOR --> ^
+// NOT --> ~ --> visit later
+
+// 11->1
+// 01->0
+// 10->0
+// 00->0
+
+//8421 -->
+
+
+
+
+
+
+let marks = 61;
+let passMarks = 31
+// let timeTaken = 30;
+let result;
+// if(true){
+//     // this will be executed
+// } else{
+//     // not true then this
+// }
+
+console.log(71>70);
+
+if(marks > passMarks){
+    result = 'pass'
+    console.log('student passed'); //1
+}else{
+    result = 'failed'
+    console.log('student failed'); //2
+}
+
+if(passMarks === 30){
+    console.log('pass marks is minimal');
+}
+
+console.log('result is ', result);
+
+
+
+
+
+
+
