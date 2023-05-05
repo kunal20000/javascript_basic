7b7f27f43aa5b08c34f620c482276c046efa5d70
kunal20000 <kunalsapate2000@gmail.com>
2023-05-06 00:30:40 +0530

    Js

--- .idea/JS_Basic/Function2.js
+++ .idea/JS_Basic/Function2.js
@@ -2,20 +2,20 @@ console.log("function");
 //Hoisting in JS
 
 //console.log(x); //undefined
-console.log(printMyName);// he print all function
+//console.log(printMyName);// he print all function
 //console.log(calculate); //undefined
 
-printMyName(); // he prints the output of function
+//printMyName(); // he prints the output of function
 //calculate();
 
 
-var x =10;
-console.log(x); //Type error is not a function
+//var x =10;
+//console.log(x); //Type error is not a function
 
 
-function printMyName(){
-console.log('Printing name');
-}
+//function printMyName(){
+//console.log('Printing name');
+//}
 
 //var calculate = function(){
 //console.log('name');
@@ -62,38 +62,144 @@ console.log('Printing name');
 // and it gives error ref error "cant acces before "initilization"
 // temporal dead zone
 
-let a =10;
-console.log('after denining a');
-console.log(a);
-a=20;
-console.log(a);
+//let a =10;
+//console.log('after denining a');
+//console.log(a);
+//a=20;
+//console.log(a);
 
-const b =30;
+//const b =30;
 //b=60;
 
-function sum(x,y){
-console.log(x+y);
-}
-sum(2,7);
-
-let multiply= function(x,y){
-console.log(x*y);
-}
-
-multiply(6,7);
-multiply("6","8");
+//function sum(x,y){
+//console.log(x+y);
+//}
+//sum(2,7);
+//
+//let multiply= function(x,y){
+//console.log(x*y);
+//}
+//
+//multiply(6,7);
+//multiply("6","8");
 
 //first class citizen in JS
 // store function in variable
 // we can pass function as arguments to function
 //we can also receive function as arguments
 
-let calculate = function(value, fun){
-  console.log(value);
-  fun(value)
+//let calculate = function(value, fun){
+//  console.log(value);
+//  fun(value)
+//}
+//function square(no){
+//console.log(no*no);
+//}
+//calculate(10, square)
+
+
+const userinfo = {
+  name: 'abhinav',
+  teaching: true,
+  printNameAndProfession: function(){
+    console.log("printing name and profession");
+  }
 }
+
+// userInfo.name
+
+userinfo.printNameAndProfession();
+
+let calculate = function(no , fun){
+console.log(no);
+console.log(fun);
+let a =100;
+let b =40;
+
+let multi =a*b;
+//fun(multi)
+}
+//
+//let no = 30;
+//let fun = function(value){
+//console.log('value received is', value);
+//}
+
 function square(no){
 console.log(no*no);
 }
-calculate(10, square)
 
+calculate(10,square);// square is callback function
+
+calculate(30, function(value){
+console.log('value received is', value);
+})//callback function anonyms function
+
+
+// calculate --> higher order function -->
+// when a function takes a function as argument
+// when a function return a function from it
+// or both
+
+
+function returnFun(){
+let a =10;
+return function(){
+ console.log('calculating result');
+}
+//console.log(a);
+//return a;
+}
+
+let result  = returnFun();
+console.log(result); // the whole function printed
+result();
+
+
+
+// first class function
+// anonyms functions // this function has not name
+// higher order functions --> only possible because of first class fun
+// callback functions
+
+// Arrow function
+
+let printName = (myName) =>{
+ let a=100;
+ let b = 100;
+ console.log(a+b);
+ console.log('myNameIs', myName);
+}
+
+printName('kunal');
+printName('Aswath');
+
+let getValue1 = ()=> 100
+
+
+let result1 = getValue1();
+console.log(result1);
+
+let getValue2 = (num, douFun)=>{
+// return douFun;
+//
+// let res = douFun(num);
+// return res;
+ return douFun(num); //return 40
+}
+
+
+let result2= getValue2(20,(no)=> 2*no);
+
+//let res = getValue2(20,(no)=>{
+//  return 2*no;
+//})
+console.log(res);
+
+
+
+
+// let a = 30
+// let myFync = function(){
+
+// }
