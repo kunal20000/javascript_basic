d4f34d5c0201f10babce5e51dfba23d0e83edbe5
kunal20000 <kunalsapate2000@gmail.com>
2023-05-14 01:19:08 +0530

    Js string

---
+++ src/Assignment/Js_String_1.js
@@ -0,0 +1,11 @@
+function numOfWords(String){
+    // write code here
+    const words = String.split(" ");
+
+    const numOfWords = words.length;
+
+    return numOfWords;
+
+    // return the output , do not use console.log here
+
+}
