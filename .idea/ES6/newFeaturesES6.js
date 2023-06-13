4ffc88cc61d88f2bd9168711ca640bb34a4834bb
kunal20000 <kunalsapate2000@gmail.com>
2023-06-13 20:42:12 +0530

    Async

---
+++ .idea/ES6/newFeaturesES6.js
@@ -0,0 +1,15 @@
+// ES6 features
+
+// classes
+// Array methods --> map, filter, reduce, some, every
+// default parameters in functions
+// rest and spread operators(...)
+// template literals(`${}`)
+// we got two new ways of declaring variables (let and const) previously we were using var
+// arrow functions
+// Destructuring
+// ES6 modules --> React
+// promises
+
+// Destructuring
+
