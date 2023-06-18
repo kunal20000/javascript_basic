48cd84d0f7699f4f3e41d5fdd3691ad7b1ece2f0
kunal <kunalsapate2000@gmail.com>
2023-06-18 23:05:47 +0530

    promises

---
+++ src/Promise/JS_Promise_1.js
@@ -0,0 +1,10 @@
+function promiseMe(time, data){
+    // return the output using return keyword
+    // do not console.log it
+   return new Promise((resolve)=>{
+       setTimeout(()=>{
+           resolve(data);
+       }, time);
+   });
+
+}
